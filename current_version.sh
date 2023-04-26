57533184ffbdfc1f1c3ddc72d528e02b86d8410d
Aida Alján Porto <aida.aljan@nomasystems.com>
2023-04-26 14:50:13 +0200

    fix(#5): Fix file update (WIP)

--- current_version.sh
+++ current_version.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 last_tag=$(git describe --tags --abbrev=0)
-echo $last_tag
 if [[ $last_tag == "latest" ]]
 then
   last_tag=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
-  echo $(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=4`)
+  last_tag=v1.7.0
 fi
 prefix=$1
 suffix=$2

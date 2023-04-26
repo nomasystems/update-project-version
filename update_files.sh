415b4fc60b9f212d294b7b8a9c37a30e97c5a098
AidaAP <87307170+AidaAP@users.noreply.github.com>
2023-04-26 16:04:15 +0200

    fix(#5): Fix file update (#6)
    closes #5

--- update_files.sh
+++ update_files.sh
@@ -8,15 +8,18 @@ then
   echo "No files to update"
 else
 
-  current_version=$2
-  new_version=$3
+  escaped_dot="\."
+  current_version="$2"
+  escaped_current_version="${current_version//./"$escaped_dot"}"
+  new_version="$3"
+  escaped_new_version="${new_version//./"$escaped_dot"}"
   author=$4
   echo "Updating version in files \"$files_str\" from \"$current_version\" to \"$new_version\""
 
   for file in ${files[@]}
   do
     echo "Updating file \"$file\""
-  	sed -i.bak "s/$current_version/$new_version/" $file
+  	sed -i.bak "s/$escaped_current_version/$escaped_new_version/" $file
   	rm $file".bak"
   done
 
